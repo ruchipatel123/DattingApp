3bcbbf171dcf7c4fa11fe446e33281587f8fdd91
webuplers <web@uplers.com>
2024-03-07 18:17:16 +0530

    add min date in datepicker

--- src/components/DatePicker/Datepicker.tsx
+++ src/components/DatePicker/Datepicker.tsx
@@ -4,12 +4,13 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const Datepicker = ({ selectedDate, onChange }) => {
+  console.log(moment().subtract('18Y').toDate());
   return (
     <DatePicker
       selected={selectedDate}
       onChange={onChange}
       dateFormat="dd/MM/yyyy"
-      minDate={moment().subtract('18Y').toDate()}
+      maxDate={moment().subtract(18, 'years').toDate()}
       className="focus:shadow-outline block h-[60px] w-full rounded-lg border border-gray-400 bg-transparent  px-4 py-2 text-md leading-tight shadow placeholder:text-gray-400 focus:outline-none"
       placeholderText="Select birthdate"
     />
